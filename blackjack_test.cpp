7489a85d704caee13305300ce016d8d25ca3d20c
savilovaae <a.savilova@g.nsu.ru>
2023-12-14 15:23:53 +0700

    first test added

--- blackjack_test.cpp
+++ blackjack_test.cpp
@@ -5,9 +5,25 @@
 #include "Player.h"
 
 TEST(Card, constructor) {
+    Card defaultCard;
+    EXPECT_EQ(defaultCard.getSuit(), Card::Suit::Hearts);
+    EXPECT_EQ(defaultCard.getFace(), Card::Face::Ace);
+    EXPECT_EQ(defaultCard.getValue(), 1);
+
     Card AceDiamonds(Card::Suit::Diamonds, Card::Face::Ace);
     EXPECT_EQ(AceDiamonds.getSuit(), Card::Suit::Diamonds);
     EXPECT_EQ(AceDiamonds.getFace(), Card::Face::Ace);
+    EXPECT_EQ(AceDiamonds.getValue(), 1);
+
+    Card QueenClubs(Card::Suit::Clubs, Card::Face::Queen);
+    EXPECT_EQ(QueenClubs.getSuit(), Card::Suit::Clubs);
+    EXPECT_EQ(QueenClubs.getFace(), Card::Face::Queen);
+    EXPECT_EQ(QueenClubs.getValue(), 10);
+
+    Card NineSpades(Card::Suit::Spades, Card::Face::Nine);
+    EXPECT_EQ(NineSpades.getSuit(), Card::Suit::Spades);
+    EXPECT_EQ(NineSpades.getFace(), Card::Face::Nine);
+    EXPECT_EQ(NineSpades.getValue(), 9);
 }
 
 
